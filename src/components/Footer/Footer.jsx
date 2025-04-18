5555b5c9e9a16b293e61a73177c79e848ca1422b
mbdullah <mdabdullah162005@gmail.com>
2025-04-19 03:09:18 +0600

    added the footer section, please sent it

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,29 +1,24 @@
 import React from 'react';
+import { IoLogoTwitter } from "react-icons/io5";
+import { FaYoutube } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
-  <nav>
-    <h6 className="footer-title">Services</h6>
-    <a className="link link-hover">Branding</a>
-    <a className="link link-hover">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
-  </nav>
-  <nav>
-    <h6 className="footer-title">Company</h6>
-    <a className="link link-hover">About us</a>
-    <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
-  </nav>
-  <nav>
-    <h6 className="footer-title">Legal</h6>
-    <a className="link link-hover">Terms of use</a>
-    <a className="link link-hover">Privacy policy</a>
-    <a className="link link-hover">Cookie policy</a>
-  </nav>
-</footer>
+     <div className='mb-10 bg-[#F3F3F3] py-10 lg:py-20'>
+      <div className='text-center justify-center'>
+        <img className='w-30 h-30 mx-auto mb-3' src="../../../assets/headerbook.png" alt="" />
+        <h4 className='font-bold text-2xl mb-2'>Book Vib Industries Ltd.</h4>
+        <h4 className='font-semibold text-xl mb-5'>Providing reliable tech since 1992</h4>
+        <p className='text-lg'>Copyright © 2025 - All right reserved</p>
+        <span className='flex gap-5 justify-center mt-10'>
+        <IoLogoTwitter size={30}/>
+        <FaYoutube size={30}/>
+        <FaFacebookF size={30}/>
+        </span>
+
+      </div>
+     </div>
     );
 };
 
